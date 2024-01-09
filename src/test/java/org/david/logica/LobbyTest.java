2e03be86a4ccd6ecf4d6d0a8da08bbac989a320c
Mariano Navas <marianudo@gmail.com>
2024-01-09 12:58:31 +0100

    [TEST] Probar que el lobby crea sucesivas partidas por cada batch de jugadores
    Añadimos un test que comprueba si, una vez añadido el primer batch de
    (en nuestro caso 5) jugadores, los siguientes son añadidos a una nueva
    partida.
    
    Como era de esperar, la implementación de Lobby en este commit falla
    en este segundo punto.

--- src/test/java/org/david/logica/LobbyTest.java
+++ src/test/java/org/david/logica/LobbyTest.java
@@ -8,25 +8,49 @@ import java.util.List;
 import java.util.Optional;
 
 public class LobbyTest {
+    Lobby lobby = new Lobby();
+
+    private void verificarBatchJugadores(List<IJugador> jugadores) {
+        List<Optional<Partida>> optionalList = jugadores.stream().map(lobby::anadirJugador).toList();
+
+        Assertions.assertTrue(optionalList.getLast().isPresent());
+
+        List<Optional<Partida>> allMinusLast = optionalList.subList(0, optionalList.size() - 1);
+        Assertions.assertTrue(allMinusLast.stream().allMatch(Optional::isEmpty));
+    }
 
     @Test
     void lobbyDevuelvePartidaAlAniadir5Jugadores(){
-        Lobby lobby = new Lobby();
+
         List<IJugador> jugadores = Arrays.asList(
                 new JugadorLobby("David"),new JugadorLobby("Mariano"),
                 new JugadorLobby("Gema"),new JugadorLobby("Jose"),
                 new JugadorLobby("Angel")
         );
 
-        List<Optional<Partida>> optionalList = jugadores.stream().map(lobby::anadirJugador).toList();
-
-        Assertions.assertTrue(optionalList.getLast().isPresent());
-
-        List<Optional<Partida>> allMinusLast = optionalList.subList(0, optionalList.size());
-        Assertions.assertTrue(allMinusLast.stream().allMatch(Optional::isEmpty));
+        verificarBatchJugadores(jugadores);
     }
 
+    @Test
+    void unaVezDevueltaLaPrimeraPartidaElLobbyCreaUnaSegunda() {
+        // Añadimos los primeros 5 jugadores
+        List<IJugador> jugadores = Arrays.asList(
+                new JugadorLobby("David"),new JugadorLobby("Mariano"),
+                new JugadorLobby("Gema"),new JugadorLobby("Jose"),
+                new JugadorLobby("Angel")
+        );
 
+        jugadores.forEach(lobby::anadirJugador);
+
+        // Añadimos ahora otros 5 jugadores, y comprobamos lo mismo que en el test anterior
+        List<IJugador> sigBatchJugadores = Arrays.asList(
+                new JugadorLobby("Pepe"),new JugadorLobby("Jose"),
+                new JugadorLobby("Manolo"),new JugadorLobby("El Del Bolo"),
+                new JugadorLobby("Angel hermano")
+        );
+
+        verificarBatchJugadores(sigBatchJugadores);
+    }
 }
 
 class JugadorLobby implements IJugador{
