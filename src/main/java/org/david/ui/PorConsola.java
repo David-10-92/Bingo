eb2bb1ade5b89ddaad502046c18d1c9ebef5ad66
David Cazalla <ncedcb@outlook.es>
2024-01-16 11:06:06 +0100

    Jugar por consola

--- src/main/java/org/david/ui/PorConsola.java
+++ src/main/java/org/david/ui/PorConsola.java
@@ -1,11 +1,9 @@
 package org.david.ui;
 
-import org.david.logica.GeneracionNumeros;
-import org.david.logica.IJugador;
-import org.david.logica.IPartida;
-import org.david.logica.Lobby;
+import org.david.logica.*;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 public class PorConsola {
@@ -13,16 +11,17 @@ public class PorConsola {
     private final static GeneracionNumeros generarNumeros = new GeneracionNumeros();
     private final static Lobby lobby = new Lobby(generarNumeros);
 
+
     public static void main(String[] args) {
 
-        lobby.añadirJugador(IJugador.crearJugador("David",2));
+        lobby.añadirJugador(IJugador.crearJugador("David",1));
         lobby.añadirJugador(IJugador.crearJugador("Gema",1));
         lobby.añadirJugador(IJugador.crearJugador("Mariano",2));
         lobby.añadirJugador(IJugador.crearJugador("Maria",1));
         Optional<IPartida> comienza = lobby.añadirJugador(IJugador.crearJugador("Antonio",1));
 
-        IJugador jugadorGana = comienza.get().jugar();
-        System.out.println(jugadorGana.getNombre());
+        Map<EstadoPartida,IJugador> jugadorGana = comienza.get().jugar();
+        System.out.println(jugadorGana);
     }
 
 }
